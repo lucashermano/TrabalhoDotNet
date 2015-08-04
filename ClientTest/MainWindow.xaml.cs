6c861873d88b658520938ccc93bfdeb5cceb31f4
lucashermano <B-014mut014>
2015-08-04 17:30:33 -0300

    Arquivos que ficaram de fora do ultimo commit da criação do projeto ClientTest

---
+++ ClientTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ModeloCanonico;
+using System;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Windows;
+
+namespace ClientTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReferenceKgb.KgbServiceClient kgb = new ServiceReferenceKgb.KgbServiceClient();
+            ModeloCanonico.Custumer custumer = kgb.GetCustomerByCPF("04986491644");
+            textBox.Text = custumer.Nome;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReferenceApt.AptServiceClient apt = new ServiceReferenceApt.AptServiceClient();
+
+            ModeloCanonico.Custumer custumer = new ModeloCanonico.Custumer();
+            custumer.Nome = "Custumer apt";
+            ModeloCanonico.Acount acount = new ModeloCanonico.Acount();
+            acount.Number = "122345";
+            ModeloCanonico.Portability portabilidade = apt.SolicitarBilhetePortabilidade(custumer, acount);
+            textBox1.Text = portabilidade.Bilhete;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "0";
+            //[chamando o webservice do módulo Anatel]
+            
+            Anatel.IAnatel client;
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (Anatel.IAnatel)Activator.GetObject(typeof(Anatel.IAnatel), "tcp://localhost:8091/SolicitarPortabilidadeNumerica");
+            Custumer custumer = new Custumer();
+            custumer.Nome = "Hermano";
+            //[obtendo resposta da Anatel para a portabilidade]
+            RetornoPortabilidade retorno = client.SolicitarPortabilidadeNumerica(custumer);
+            textBox2.Text = retorno.Bilhete;
+        }
+    }
+}
