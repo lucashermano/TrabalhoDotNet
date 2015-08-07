c23584b57f8b2dec88da9ec4ea4b2e2abef0e0d6
lucashermano <B-014mut014>
2015-08-07 20:42:55 -0300

    Criação do teste unitario dos projetos Anatel e Apt.

--- ClientTest/TesteUnitarioServiceAnatel.xaml.cs
+++ ClientTest/TesteUnitarioServiceAnatel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModeloCanonico;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
@@ -33,7 +34,50 @@ namespace ClientTest
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            Custumer custumer = new Custumer();
+            custumer.Nome = "cliente com teste de sucesso";
+            custumer.Cpf = "88888888888";
+            RetornoPortabilidade retorno = client.SolicitarPortabilidadeNumerica(custumer);
+            if (retorno.CodigoErro == "0")
+            {
+                textBoxSucesso.Text = "Sucesso na chamada ao Anatel. Código: "+retorno.CodigoErro+" - Mensagem retorno:  "+
+                    retorno.Motivo+" - Bilhete gerado: "+retorno.Bilhete;
+            } else
+            {
+                textBoxSucesso.Text = "Erro na chamada ao Anatel.";
+            }
+        }
 
+        private void buttonErro01_Click(object sender, RoutedEventArgs e)
+        {
+            Custumer custumer = new Custumer();
+            custumer.Nome = "cliente erro 01";
+            custumer.Cpf = "04986491644";
+            RetornoPortabilidade retorno = client.SolicitarPortabilidadeNumerica(custumer);
+            if (retorno.CodigoErro == "1")
+            {
+                textBoxErro01.Text = "Erro Código: " + retorno.CodigoErro + " - Mensagem retorno: " + retorno.Motivo;
+            }
+            else
+            {
+                textBoxErro01.Text = "Erro na chamada ao Anatel.";
+            }
+        }
+
+        private void buttonErro02_Click(object sender, RoutedEventArgs e)
+        {
+            Custumer custumer = new Custumer();
+            custumer.Nome = "cliente erro 02";
+            custumer.Cpf = "05666561677";
+            RetornoPortabilidade retorno = client.SolicitarPortabilidadeNumerica(custumer);
+            if (retorno.CodigoErro == "2")
+            {
+                textBoxErro02.Text = "Erro Código: " + retorno.CodigoErro + " - Mensagem retorno: " + retorno.Motivo;
+            }
+            else
+            {
+                textBoxErro02.Text = "Erro na chamada ao Anatel.";
+            }
         }
     }
 }
---
+++ ClientTest/TesteUnitarioServiceApt.xaml.cs
@@ -0,0 +1,73 @@
+﻿using ModeloCanonico;
+using System;
+using System.ServiceModel;
+using System.Windows;
+
+namespace ClientTest
+{
+    /// <summary>
+    /// Interaction logic for TesteUnitarioServiceAnatel.xaml
+    /// </summary>
+    public partial class TesteUnitarioServiceApt : Window
+    {
+        ServiceReferenceApt.AptServiceClient apt = new ServiceReferenceApt.AptServiceClient();
+
+        public TesteUnitarioServiceApt()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ModeloCanonico.Custumer custumer = new ModeloCanonico.Custumer();
+            custumer.Cpf = "88888888888";
+            ModeloCanonico.Acount acount = new ModeloCanonico.Acount();
+            acount.Number = "122345";
+            ModeloCanonico.Portability portabilidade = apt.SolicitarBilhetePortabilidade(custumer, acount);
+            textBoxSucesso.Text = "Sucesso na chamada ao APT. Número do bilhete: "+portabilidade.Bilhete;
+        }
+
+        private void buttonErro01_Click(object sender, RoutedEventArgs e)
+        {
+            ModeloCanonico.Custumer custumer = new ModeloCanonico.Custumer();
+            custumer.Cpf = "04986491644";
+            ModeloCanonico.Acount acount = new ModeloCanonico.Acount();
+            acount.Number = "122345";
+            try
+            {
+                ModeloCanonico.Portability portabilidade = apt.SolicitarBilhetePortabilidade(custumer, acount);
+                textBoxErro01.Text = "Não lançou exceção corretamente.";
+            }
+            catch (FaultException<PortabilidadeFault> erro)
+            {
+                textBoxErro01.Text = "Erro "+erro.Detail.CodigoErro+": " + erro.Detail.Motivo;
+            }
+            catch (Exception erroGenerico)
+            {
+                textBoxErro01.Text = "Erro de chamada ao webservice: "+ erroGenerico;
+            }
+            
+        }
+
+        private void buttonErro02_Click(object sender, RoutedEventArgs e)
+        {
+            ModeloCanonico.Custumer custumer = new ModeloCanonico.Custumer();
+            custumer.Cpf = "05666561677";
+            ModeloCanonico.Acount acount = new ModeloCanonico.Acount();
+            acount.Number = "122345";
+            try
+            {
+                ModeloCanonico.Portability portabilidade = apt.SolicitarBilhetePortabilidade(custumer, acount);
+                textBoxErro02.Text = "Não lançou exceção corretamente.";
+            }
+            catch (FaultException<PortabilidadeFault> erro)
+            {
+                textBoxErro02.Text = "Erro " + erro.Detail.CodigoErro + ": " + erro.Detail.Motivo;
+            }
+            catch (Exception erroGenerico)
+            {
+                textBoxErro02.Text = "Erro de chamada ao webservice: " + erroGenerico;
+            }
+        }
+    }
+}
